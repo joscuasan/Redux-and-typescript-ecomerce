e8ea9b04894b24bd007728051fcc0dd23d6be41a
joscuasan <156707396+joscuasan@users.noreply.github.com>
2025-01-26 17:59:59 +0100

    Add files via upload

---
+++ src/dummy-products.ts
@@ -0,0 +1,58 @@
+import dishSoap from './assets/dishSoap.jpg';
+import glassCleaner from './assets/glassCleaner.jpg';
+import bleach from './assets/bleach.jpg';
+// import merlotSuit from './assets/merlot-suit.jpg';
+// import moonlightDress from './assets/moonlight-dress.jpg';
+// import denimPioneer from './assets/denim-pioneer.jpg';
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 'd1',
+    image: dishSoap,
+    title: 'Lemon Fresh Dish Soap',
+    price: 4.99,
+    description:
+      'Tackle greasy dishes effortlessly with our Lemon Fresh Dish Soap. Gentle on hands, tough on grease!',
+  },
+  {
+    id: 'd2',
+    image: glassCleaner,
+    title: 'Crystal Clear Glass Cleaner',
+    price: 3.99,
+    description:
+      'Achieve streak-free shine with our Crystal Clear Glass Cleaner, perfect for mirrors, windows, and more.',
+  },
+  {
+    id: 'd3',
+    image: bleach,
+    title: 'Ultra Strength Bleach',
+    price: 2.99,
+    description:
+      'Keep your whites brighter and your surfaces sanitized with our Ultra Strength Bleach.',
+  }
+  // },
+  // {
+  //   id: 'd4',
+  //   image: laundryDetergent,
+  //   title: 'Fresh Breeze Laundry Detergent',
+  //   price: 9.99,
+  //   description:
+  //     'Enjoy soft, clean, and wonderfully fresh-smelling clothes with our Fresh Breeze Laundry Detergent.',
+  // },
+  // {
+  //   id: 'd5',
+  //   image: mopCleaner,
+  //   title: 'Lavender Floor Mop Cleaner',
+  //   price: 6.99,
+  //   description:
+  //     'Clean and refresh your floors with our Lavender Mop Cleaner, leaving a subtle, calming scent.',
+  // },
+  // {
+  //   id: 'd6',
+  //   image: airFreshener,
+  //   title: 'Citrus Burst Air Freshener',
+  //   price: 2.49,
+  //   description:
+  //     'Revitalize your space with the zesty aroma of our Citrus Burst Air Freshener, perfect for any room.',
+  // },
+];
